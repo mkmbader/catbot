78a512320993c0a61f4a6bb0be128f95555242ac
Maria Bader <you@example.com>
2025-09-05 11:19:10 +0200

    openai embeddings model

--- catbot/database/embedding.py
+++ catbot/database/embedding.py
@@ -2,14 +2,31 @@
 
 import chromadb
 from langchain.schema import Document
+import chromadb.utils.embedding_functions as embedding_functions
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Loads .env file into environment variables
+
+API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+def embedding_function(model_name: str) -> embedding_functions.OpenAIEmbeddingFunction:
+    """Creates an embedding function using OpenAI's API."""
+
+    return embedding_functions.OpenAIEmbeddingFunction(
+        api_key=API_KEY, model_name=model_name
+    )
 
 
 def create_data_base(
     sections: list[Document],
-    embeddings_function,
+    model_name="text-embedding-3-small",
     db_file_path: str = "catbot/database/chroma",
 ):
     """Creates a ChromaDB database and stores the documents with their embeddings."""
+
+    embeddings_function = embedding_function(model_name=model_name)
 
     chroma_client = chromadb.PersistentClient(path=db_file_path)
 
